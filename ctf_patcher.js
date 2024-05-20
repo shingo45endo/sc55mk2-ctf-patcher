3add7e53a6235e6777c8fbc127be1e190f12725b
shingo45endo <shingo45endo@gmail.com>
2024-05-20 22:18:51 +0900

    Support compatibility options

--- ctf_patcher.js
+++ ctf_patcher.js
@@ -1,5 +1,646 @@
-export function modifyToneTable(allBytes) {
+const sc55tones = {
+	0: {
+		0: 'Piano 1',
+		126: 'Piano 2',
+		127: 'Acou Piano 1',
+	},
+	1: {
+		0: 'Piano 2',
+		126: 'Piano 2',
+		127: 'Acou Piano 2',
+	},
+	2: {
+		0: 'Piano 3',
+		126: 'Piano 2',
+		127: 'Acou Piano 3',
+	},
+	3: {
+		0: 'Honky-tonk',
+		126: 'Honky-tonk',
+		127: 'Elec Piano 1',
+	},
+	4: {
+		0: 'E.Piano 1',
+		8: 'Detuned EP 1',
+		126: 'Piano 1',
+		127: 'Elec Piano 2',
+	},
+	5: {
+		0: 'E.Piano 2',
+		8: 'Detuned EP 2',
+		126: 'Piano 2',
+		127: 'Elec Piano 3',
+	},
+	6: {
+		0: 'Harpsichord',
+		8: 'Coupled Hps.',
+		126: 'Piano 2',
+		127: 'Elec Piano 4',
+	},
+	7: {
+		0: 'Clav.',
+		126: 'E.Piano 1',
+		127: 'Honkytonk',
+	},
+	8: {
+		0: 'Celesta',
+		126: 'Detuned EP 1',
+		127: 'Elec Org 1',
+	},
+	9: {
+		0: 'Glockenspiel',
+		126: 'E.Piano 2',
+		127: 'Elec Org 2',
+	},
+	10: {
+		0: 'Music Box',
+		126: 'Steel-str.Gt',
+		127: 'Elec Org 3',
+	},
+	11: {
+		0: 'Vibraphone',
+		126: 'Steel-str.Gt',
+		127: 'Elec Org 4',
+	},
+	12: {
+		0: 'Marimba',
+		126: '12-str.Gt',
+		127: 'Pipe Org 1',
+	},
+	13: {
+		0: 'Xylophone',
+		126: 'Funk Gt.',
+		127: 'Pipe Org 2',
+	},
+	14: {
+		0: 'Tubular-bell',
+		8: 'Church Bell',
+		126: 'Muted Gt.',
+		127: 'Pipe Org 3',
+	},
+	15: {
+		0: 'Santur',
+		126: 'Slap Bass 1',
+		127: 'Accordion',
+	},
+	16: {
+		0: 'Organ 1',
+		8: 'Detuned Or.1',
+		126: 'Slap Bass 1',
+		127: 'Harpsi 1',
+	},
+	17: {
+		0: 'Organ 2',
+		8: 'Detuned Or.2',
+		126: 'Slap Bass 1',
+		127: 'Harpsi 2',
+	},
+	18: {
+		0: 'Organ 3',
+		126: 'Slap Bass 1',
+		127: 'Harpsi 3',
+	},
+	19: {
+		0: 'Church Org.1',
+		8: 'Church Org.2',
+		126: 'Slap Bass 2',
+		127: 'Clavi 1',
+	},
+	20: {
+		0: 'Reed Organ',
+		126: 'Slap Bass 2',
+		127: 'Clavi 2',
+	},
+	21: {
+		0: 'Accordion Fr',
+		8: 'Accordion It',
+		126: 'Slap Bass 2',
+		127: 'Clavi 3',
+	},
+	22: {
+		0: 'Harmonica',
+		126: 'Slap Bass 2',
+		127: 'Celesta 1',
+	},
+	23: {
+		0: 'Bandneon',
+		126: 'Fingered Bs.',
+		127: 'Celesta 2',
+	},
+	24: {
+		0: 'Nylon-str.Gt',
+		8: 'Ukulele',
+		126: 'Fingered Bs.',
+		127: 'Syn Brass 1',
+	},
+	25: {
+		0: 'Steel-str.Gt',
+		8: '12-str.Gt',
+		16: 'Mandolin',
+		126: 'Picked Bs.',
+		127: 'Syn Brass 2',
+	},
+	26: {
+		0: 'Jazz Gt.',
+		8: 'Hawaiian Gt.',
+		126: 'Picked Bs.',
+		127: 'Syn Brass 3',
+	},
+	27: {
+		0: 'Clean Gt.',
+		8: 'Chorus Gt.',
+		126: 'Fretless Bs.',
+		127: 'Syn Brass 4',
+	},
+	28: {
+		0: 'Muted Gt.',
+		8: 'Funk Gt.',
+		126: 'Acoustic Bs.',
+		127: 'Syn Bass 1',
+	},
+	29: {
+		0: 'Overdrive Gt',
+		126: 'Choir Aahs',
+		127: 'Syn Bass 2',
+	},
+	30: {
+		0: 'DistortionGt',
+		8: 'Feedback Gt.',
+		126: 'Choir Aahs',
+		127: 'Syn Bass 3',
+	},
+	31: {
+		0: 'Gt.Harmonics',
+		8: 'Gt. Feedback',
+		126: 'Choir Aahs',
+		127: 'Syn Bass 4',
+	},
+	32: {
+		0: 'Acoustic Bs.',
+		126: 'Choir Aahs',
+		127: 'Fantasy',
+	},
+	33: {
+		0: 'Fingered Bs.',
+		126: 'Slow Strings',
+		127: 'Harmo Pan',
+	},
+	34: {
+		0: 'Picked Bs.',
+		126: 'Strings',
+		127: 'Chorale',
+	},
+	35: {
+		0: 'Fretless Bs.',
+		126: 'Syn.Strings3',
+		127: 'Glasses',
+	},
+	36: {
+		0: 'Slap Bass 1',
+		126: 'Syn.Strings3',
+		127: 'Soundtrack',
+	},
+	37: {
+		0: 'Slap Bass 2',
+		126: 'Organ 1',
+		127: 'Atmosphere',
+	},
+	38: {
+		0: 'Synth Bass 1',
+		8: 'Synth Bass 3',
+		126: 'Organ 1',
+		127: 'Warm Bell',
+	},
+	39: {
+		0: 'Synth Bass 2',
+		8: 'Synth Bass 4',
+		126: 'Organ 1',
+		127: 'Funny Vox',
+	},
+	40: {
+		0: 'Violin',
+		126: 'Organ 2',
+		127: 'Echo Bell',
+	},
+	41: {
+		0: 'Viola',
+		126: 'Organ 1',
+		127: 'Ice Rain',
+	},
+	42: {
+		0: 'Cello',
+		126: 'Organ 1',
+		127: 'Oboe 2001',
+	},
+	43: {
+		0: 'Contrabass',
+		126: 'Organ 2',
+		127: 'Echo Pan',
+	},
+	44: {
+		0: 'Tremolo Str',
+		126: 'Organ 2',
+		127: 'Doctor Solo',
+	},
+	45: {
+		0: 'PizzicatoStr',
+		126: 'Organ 2',
+		127: 'School Daze',
+	},
+	46: {
+		0: 'Harp',
+		126: 'Trumpet',
+		127: 'Bellsinger',
+	},
+	47: {
+		0: 'Timpani',
+		126: 'Trumpet',
+		127: 'Square Wave',
+	},
+	48: {
+		0: 'Strings',
+		8: 'Orchestra',
+		126: 'Trombone',
+		127: 'Str Sect 1',
+	},
+	49: {
+		0: 'Slow Strings',
+		126: 'Trombone',
+		127: 'Str Sect 2',
+	},
+	50: {
+		0: 'Syn.Strings1',
+		8: 'Syn.Strings3',
+		126: 'Trombone',
+		127: 'Str Sect 3',
+	},
+	51: {
+		0: 'Syn.Strings2',
+		126: 'Trombone',
+		127: 'Pizzicato',
+	},
+	52: {
+		0: 'Choir Aahs',
+		126: 'Trombone',
+		127: 'Violin 1',
+	},
+	53: {
+		0: 'Voice Oohs',
+		126: 'Trombone',
+		127: 'Violin 2',
+	},
+	54: {
+		0: 'SynVox',
+		126: 'Alto Sax',
+		127: 'Cello 1',
+	},
+	55: {
+		0: 'OrchestraHit',
+		126: 'Tenor Sax',
+		127: 'Cello 2',
+	},
+	56: {
+		0: 'Trumpet',
+		126: 'Baritone Sax',
+		127: 'Contrabass',
+	},
+	57: {
+		0: 'Trombone',
+		126: 'Alto Sax',
+		127: 'Harp 1',
+	},
+	58: {
+		0: 'Tuba',
+		126: 'Brass 1',
+		127: 'Harp 2',
+	},
+	59: {
+		0: 'MutedTrumpet',
+		126: 'Brass 1',
+		127: 'Guitar 1',
+	},
+	60: {
+		0: 'French Horn',
+		126: 'Brass 2',
+		127: 'Guitar 2',
+	},
+	61: {
+		0: 'Brass 1',
+		8: 'Brass 2',
+		126: 'Brass 2',
+		127: 'Elec Gtr 1',
+	},
+	62: {
+		0: 'Synth Brass1',
+		8: 'Synth Brass3',
+		126: 'Brass 1',
+		127: 'Elec Gtr 2',
+	},
+	63: {
+		0: 'Synth Brass2',
+		8: 'Synth Brass4',
+		126: 'OrchestraHit',
+		127: 'Sitar',
+	},
+	64: {
+		0: 'Soprano Sax',
+		127: 'Acou Bass 1',
+	},
+	65: {
+		0: 'Alto Sax',
+		127: 'Acou Bass 2',
+	},
+	66: {
+		0: 'Tenor Sax',
+		127: 'Elec Bass 1',
+	},
+	67: {
+		0: 'Baritone Sax',
+		127: 'Elec Bass 2',
+	},
+	68: {
+		0: 'Oboe',
+		127: 'Slap Bass 1',
+	},
+	69: {
+		0: 'English Horn',
+		127: 'Slap Bass 2',
+	},
+	70: {
+		0: 'Bassoon',
+		127: 'Fretless 1',
+	},
+	71: {
+		0: 'Clarinet',
+		127: 'Fretless 2',
+	},
+	72: {
+		0: 'Piccolo',
+		127: 'Flute 1',
+	},
+	73: {
+		0: 'Flute',
+		127: 'Flute 2',
+	},
+	74: {
+		0: 'Recorder',
+		127: 'Piccolo 1',
+	},
+	75: {
+		0: 'Pan Flute',
+		127: 'Piccolo 2',
+	},
+	76: {
+		0: 'Bottle Blow',
+		127: 'Recorder',
+	},
+	77: {
+		0: 'Shakuhachi',
+		127: 'Pan Pipes',
+	},
+	78: {
+		0: 'Whistle',
+		127: 'Sax 1',
+	},
+	79: {
+		0: 'Ocarina',
+		127: 'Sax 2',
+	},
+	80: {
+		0: 'Square Wave',
+		8: 'Sine Wave',
+		127: 'Sax 3',
+	},
+	81: {
+		0: 'Saw Wave',
+		127: 'Sax 4',
+	},
+	82: {
+		0: 'Syn.Calliope',
+		127: 'Clarinet 1',
+	},
+	83: {
+		0: 'Chiffer Lead',
+		127: 'Clarinet 2',
+	},
+	84: {
+		0: 'Charang',
+		127: 'Oboe',
+	},
+	85: {
+		0: 'Solo Vox',
+		127: 'Engl Horn',
+	},
+	86: {
+		0: '5th Saw Wave',
+		127: 'Bassoon',
+	},
+	87: {
+		0: 'Bass & Lead',
+		127: 'Harmonica',
+	},
+	88: {
+		0: 'Fantasia',
+		127: 'Trumpet 1',
+	},
+	89: {
+		0: 'Warm Pad',
+		127: 'Trumpet 2',
+	},
+	90: {
+		0: 'Polysynth',
+		127: 'Trombone 1',
+	},
+	91: {
+		0: 'Space Voice',
+		127: 'Trombone 2',
+	},
+	92: {
+		0: 'Bowed Glass',
+		127: 'Fr Horn 1',
+	},
+	93: {
+		0: 'Metal Pad',
+		127: 'Fr Horn 2',
+	},
+	94: {
+		0: 'Halo Pad',
+		127: 'Tuba',
+	},
+	95: {
+		0: 'Sweep Pad',
+		127: 'Brs Sect 1',
+	},
+	96: {
+		0: 'Ice Rain',
+		127: 'Brs Sect 2',
+	},
+	97: {
+		0: 'Soundtrack',
+		127: 'Vibe 1',
+	},
+	98: {
+		0: 'Crystal',
+		127: 'Vibe 2',
+	},
+	99: {
+		0: 'Atmosphere',
+		127: 'Syn Mallet',
+	},
+	100: {
+		0: 'Brightness',
+		127: 'Windbell',
+	},
+	101: {
+		0: 'Goblin',
+		127: 'Glock',
+	},
+	102: {
+		0: 'Echo Drops',
+		127: 'Tube Bell',
+	},
+	103: {
+		0: 'Star Theme',
+		127: 'Xylophone',
+	},
+	104: {
+		0: 'Sitar',
+		127: 'Marimba',
+	},
+	105: {
+		0: 'Banjo',
+		127: 'Koto',
+	},
+	106: {
+		0: 'Shamisen',
+		127: 'Sho',
+	},
+	107: {
+		0: 'Koto',
+		8: 'Taisho Koto',
+		127: 'Shakuhachi',
+	},
+	108: {
+		0: 'Kalimba',
+		127: 'Whistle 1',
+	},
+	109: {
+		0: 'Bag Pipe',
+		127: 'Whistle 2',
+	},
+	110: {
+		0: 'Fiddle',
+		127: 'Bottleblow',
+	},
+	111: {
+		0: 'Shanai',
+		127: 'Breathpipe',
+	},
+	112: {
+		0: 'Tinkle Bell',
+		127: 'Timpani',
+	},
+	113: {
+		0: 'Agogo',
+		127: 'Melodic Tom',
+	},
+	114: {
+		0: 'Steel Drums',
+		127: 'Deep Snare',
+	},
+	115: {
+		0: 'Woodblock',
+		8: 'Castanets',
+		127: 'Elec Perc 1',
+	},
+	116: {
+		0: 'Taiko',
+		8: 'Concert BD',
+		127: 'Elec Perc 2',
+	},
+	117: {
+		0: 'Melo. Tom 1',
+		8: 'Melo. Tom 2',
+		127: 'Taiko',
+	},
+	118: {
+		0: 'Synth Drum',
+		8: '808 Tom',
+		127: 'Taiko Rim',
+	},
+	119: {
+		0: 'Reverse Cym.',
+		127: 'Cymbal',
+	},
+	120: {
+		0: 'Gt.FretNoise',
+		1: 'Gt.Cut Noise',
+		2: 'String Slap',
+		127: 'Castanets',
+	},
+	121: {
+		0: 'Breath Noise',
+		1: 'Fl.Key Click',
+		127: 'Triangle',
+	},
+	122: {
+		0: 'Seashore',
+		1: 'Rain',
+		2: 'Thunder',
+		3: 'Wind',
+		4: 'Stream',
+		5: 'Bubble',
+		127: 'Orche Hit',
+	},
+	123: {
+		0: 'Bird',
+		1: 'Dog',
+		2: 'Horse-Gallop',
+		127: 'Telephone',
+	},
+	124: {
+		0: 'Telephone 1',
+		1: 'Telephone 2',
+		2: 'DoorCreaking',
+		3: 'Door',
+		4: 'Scratch',
+		5: 'Windchime',
+		127: 'Bird Tweet',
+	},
+	125: {
+		0: 'Helicopter',
+		1: 'Car-Engine',
+		2: 'Car-Stop',
+		3: 'Car-Pass',
+		4: 'Car-Crash',
+		5: 'Siren',
+		6: 'Train',
+		7: 'Jetplane',
+		8: 'Starship',
+		9: 'Burst Noise',
+		127: 'One Note Jam',
+	},
+	126: {
+		0: 'Applause',
+		1: 'Laughing',
+		2: 'Screaming',
+		3: 'Punch',
+		4: 'Heart Beat',
+		5: 'Footsteps',
+		127: 'Water Bell',
+	},
+	127: {
+		0: 'Gun Shot',
+		1: 'Machine Gun',
+		2: 'Lasergun',
+		3: 'Explosion',
+		127: 'Jungle Tune',
+	},
+};
+
+export function modifyToneTable(allBytes, compatMode = 'sc55mk2') {
 	console.assert(allBytes instanceof Uint8Array);
+	console.assert(['strict-sc55', 'sc55', 'sc55mk2'].includes(compatMode));
 
 	// Checks the size of the firmware ROM.
 	if (allBytes.length < 0x038000) {
@@ -15,21 +656,31 @@ export function modifyToneTable(allBytes) {
 		return p;
 	}, []);
 
+	// Defines a function to get sub-capital tone.
+	const getSubCapitalTone = (compatMode === 'strict-sc55' || compatMode === 'sc55') ?
+		(prog, bankM) => {
+			const subCapitalTone = tableTones[bankM & 0x78][prog];
+			return (subCapitalTone !== 0xffff && !sc55tones[prog][bankM]) ? 0xffff : subCapitalTone;
+		} :
+		(prog, bankM) => tableTones[bankM & 0x78][prog];
+
 	// Modifies the tone tables to support "Alternate Voicings".
 	const toneNos = [];
 	for (let bankM = 0; bankM < 64; bankM++) {	// BankM #64 and above shall not be subject to fallback because they are special use areas.
 		const tones = convertToUint16BEArray(bytes.slice(bankM * 256, (bankM + 1) * 256));
 		const newTones = tones.map((toneNo, prog) => {
-			if (toneNo !== 0xffff || prog >= 120) {	// Prog #121 (internally #120) and above shall not be subject to fallback because they are special-effects sounds.
+			// Prog #121 (internally #120) and above shall not be subject to fallback because they are special-effects sounds.
+			if (prog >= 120) {
 				return toneNo;
-			} else {
-				const subCapitalTone = tableTones[bankM & 0x78][prog];
-				if (subCapitalTone !== 0xffff) {
-					return subCapitalTone;
-				} else {
-					return tableTones[0][prog];
-				}
 			}
+			// If a tone exists in the tone table, returns its tone number.
+			// But in "Strict SC-55 mode", tones not in the original SC-55 are subject to fallback.
+			if (toneNo !== 0xffff && !(compatMode === 'strict-sc55' && !sc55tones[prog][bankM])) {
+				return toneNo;
+			}
+			// Applies fallback.
+			const subCapitalToneNo = getSubCapitalTone(prog, bankM);
+			return (subCapitalToneNo !== 0xffff) ? subCapitalToneNo : tableTones[0][prog];
 		});
 		toneNos.push(...newTones);
 	}
@@ -38,8 +689,9 @@ export function modifyToneTable(allBytes) {
 	bytes.set(convertToBytes(toneNos));
 }
 
-export function modifyDrumTable(allBytes) {
+export function modifyDrumTable(allBytes, compatMode = 'sc55v1') {
 	console.assert(allBytes instanceof Uint8Array);
+	console.assert(['sc55v1', 'sc55v2'].includes(compatMode));
 
 	// Checks the size of the firmware ROM.
 	if (allBytes.length < 0x038080) {
@@ -50,12 +702,16 @@ export function modifyDrumTable(allBytes) {
 	const tableDrumSets = allBytes.subarray(0x038000, 0x038080);
 
 	// Modifies the drum table to support "Alternate Voicings".
+	const progThreshold = (compatMode === 'sc55v1') ? 64 : 48;	// "64": SC-55 v1.00-1.21 / "48": SC-55 v2.00
 	const newTableDrumSets = tableDrumSets.map((drumSetNo, prog) => {
-		if (drumSetNo !== 0xff || prog >= 64) {	// Note: "64": SC-55 v1.00-1.21 / "48": SC-55 v2.00
+		// Prog #65 (internally #64) and above shall not be subject to fallback because they are special use areas.
+		// In v2.00, it seems that drum sets with prog #49 (internally #48) and above are defined as having "an incredible variety of sounds".
+		// Therefore, like special-effects sounds in tones, they also shall not be subject to fallback.
+		if (prog >= progThreshold) {
 			return drumSetNo;
-		} else {
-			return tableDrumSets[prog & 0x78];
 		}
+		// Applies fallback.
+		return (drumSetNo !== 0xff) ? drumSetNo : tableDrumSets[prog & 0x78];
 	});
 
 	// Writes back the modified drum table.
--- mk2ctf.js
+++ mk2ctf.js
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+import util from 'node:util';
 import process from 'node:process';
 import assert from 'node:assert';
 
@@ -6,19 +7,49 @@ import {modifyToneTable, modifyDrumTable} from './ctf_patcher.js';
 
 console.assert = assert;
 
-const binFile = fs.readFileSync(process.argv[2]);
-const bytes = new Uint8Array(binFile);
-
-if (bytes.length % 0x40000 !== 0) {
-	console.warn('The input file size is not the correct firmware ROM size.');
-}
+const options = {
+	tone: {type: 'string', choices: ['thru', 'strict-sc55', 'sc55', 'sc55mk2']},
+	drum: {type: 'string', choices: ['thru', 'sc55v1', 'sc55v2']},
+};
 
 try {
-	modifyToneTable(bytes);
-	modifyDrumTable(bytes);
+	// Parses the arguments.
+	const {values, positionals: [inputFile, outputFile]} = util.parseArgs({
+		args: process?.args ?? globalThis.Deno?.args,
+		allowPositionals: true,
+		options,
+	});
 
-	fs.writeFileSync(process.argv[3] ?? `${process.argv[2]}.new`, bytes);
+	// Checks the arguments.
+	if (!inputFile) {
+		throw new Error('Input file is not specified.');
+	}
+	for (const option of Object.keys(options)) {
+		const choices = options[option].choices;
+		if (values[option] && choices && !choices.includes(values[option])) {
+			throw new Error(`Invalid argument: "--${option}" [choices: ${choices.map((e) => `"${e}"`).join(', ')}]`);
+		}
+	}
+
+	// Reads a firmware ROM file.
+	const bytes = new Uint8Array(fs.readFileSync(inputFile));
+
+	// Checks the size of the firmware ROM.
+	if (bytes.length % 0x40000 !== 0) {
+		console.warn('The input file size is not the correct firmware ROM size.');
+	}
+
+	// Modifies the firmware ROM.
+	if (values.tone !== 'thru') {
+		modifyToneTable(bytes, values.tone);
+	}
+	if (values.drum !== 'thru') {
+		modifyDrumTable(bytes, values.drum);
+	}
+
+	// Writes a modified firmware ROM file.
+	fs.writeFileSync(outputFile ?? `${inputFile}.new`, bytes);
 
 } catch (e) {
-	console.error(`ERROR: ${e}`);
+	console.error(`ERROR: ${e.message}`);
 }
